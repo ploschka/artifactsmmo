7b8c0eee7816c617967a123fb1ce782bdb52235b
Ploshka <folga12381@gmail.com>
2024-08-07 13:04:50 +1000

    Server time added

--- api/general.lua
+++ api/general.lua
@@ -4,7 +4,12 @@ local function check_status()
     return utils.api_request('', 'GET', nil)
 end
 
+local function server_time()
+    return utils.url_request(SERVER_URL .. '/api/server-time', 'GET', nil)
+end
+
 return {
     check_status = check_status,
+    server_time = server_time,
 }
 
--- api/utils.lua
+++ api/utils.lua
@@ -28,8 +28,22 @@ local function api_request(path, method, body)
     }
 end
 
+local function url_request(url, method, body)
+    local req = create_request(url, method)
+    set_headers(req.headers, API_SECRET)
+    req:set_body(body)
+
+    local headers, stream = assert(req:go())
+    local response_body = assert(stream:get_body_as_string())
+    return {
+        status = tonumber(headers:get(':status')),
+        body = require('dkjson').decode(response_body)
+    }
+end
+
 return {
     create_request = create_request,
+    url_request = url_request,
     set_headers = set_headers,
     api_request = api_request,
 }
